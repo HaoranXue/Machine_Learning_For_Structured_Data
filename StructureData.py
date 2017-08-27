459e0e9ba55e83f071a509ce161f477cb2048de3
Haoran Xue <h.xue@ucl.ac.uk>
2017-08-28 00:07:23 +0100

    Update

--- StructureData.py
+++ StructureData.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from .Transformers import *
 
 class SData(object):
     '''
--- StructureDataFrame.py
+++ StructureDataFrame.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from .StructureData import *
+from .Transformers import *
 
 class SDataFrame(object):
     '''
